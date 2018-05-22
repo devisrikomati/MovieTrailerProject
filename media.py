b6000ffb949a76197241007ac4127a7bf495ae4c
devisrikomati <38848901+devisrikomati@users.noreply.github.com>
2018-05-22 15:52:26 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,13 +1,14 @@
-print("Content-type:text/html \n")
 import webbrowser
 
 
 class Movie():
-
-    def __init__(j,movie_title,poster_image,trailer_youtube):
+    def __init__(j, movie_title, poster_image, trailer_youtube):
         j.title = movie_title
         j.poster_image_url = poster_image
         j.trailer_youtube_url = trailer_youtube
-    def show_trailer(j):
+
+
+def show_trailer(j):
         webbrowswer.open(j.trailer_youtube_url)
+
 
